99242b2710866559dec79505f5972f5cf7d389d4
Fabian Valencia <fabian.cordoba@correounivalle.edu.co>
2024-04-20 16:26:47 -0500

    Server implemented

---
+++ src/server.js
@@ -0,0 +1,67 @@
+"use strict"
+
+const { Server } = require("socket.io");
+
+const clientURL = "http://localhost:3000"
+const port = 5000
+
+const io = new Server({
+    cors: {
+        origin: [clientURL]
+    },
+});
+
+let players = [];
+
+// Handle socket connections
+io.on('connection', (socket) => {
+    console.log(
+        "Player joined with ID",
+        socket.id, ". There are " +
+        io.engine.clientsCount +
+    " player connected."
+    );
+
+    socket.on("player-connected", () => {
+        players.push({
+            id: socket.id,
+            urlAvatar: io.engine.clientsCount === 1 ?
+                "/assets/models/avatar/Player1.glb" :
+                "/assets/models/avatar/Player2.glb",
+            position: null,
+            rotation: null,
+            animation: "Idle",
+        });
+        socket.emit("players-connected", players);
+    });
+
+    socket.on("moving-player", (valuesTransformPlayer) => {
+        const player = players.find(player => player.id === socket.id)
+        player.position = valuesTransformPlayer.position;
+        player.rotation = valuesTransformPlayer.rotation;
+        socket.broadcast.emit("updates-values-transform-player", player);
+        console.log(valuesTransformPlayer);
+    })
+
+    socket.on("change-animation", (animation) => {
+        const player = players.find(player => player.id === socket.id)
+        player.animation = animation;
+        socket.broadcast.emit("update-animation", player);
+    })
+
+    socket.on('disconnect', () => {
+        players = players.filter(player => player.id !== socket.id);
+        console.log(
+            "Player disconnected with ID",
+            socket.id, ". There are " +
+            io.engine.clientsCount +
+        " players connected");
+    });
+
+
+});
+
+// Listen on a specific port
+io.listen(port);
+
+
